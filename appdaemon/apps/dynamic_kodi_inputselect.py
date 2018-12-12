78e0522e2b80ee78c7f04b18e9254f964731da92
jkadivnik <johnny.kadivnik@esfds.com>
2018-12-12 23:37:33 +0100

    Moved input_selection to the callback method.

--- appdaemon/apps/dynamic_kodi_inputselect.py
+++ appdaemon/apps/dynamic_kodi_inputselect.py
@@ -14,8 +14,6 @@ class DynamicKodiInputSelect(hass.Hass):
         self._series_max_entries = int(self.args.get('movies_max_entries', 10))
 
         self.listen_event(self._receive_kodi_result,EVENT_KODI_CALL_METHOD_RESULT)
-        self.listen_state(self._change_selected_option, self._movie_inputselect_entity)
-        self.listen_state(self._change_selected_option, self._series_inputselect_entity)
         self._ids_options = {"Kodi Results": None}
 
     def _receive_kodi_result(self, event_id, payload_event, *args):
@@ -30,6 +28,7 @@ class DynamicKodiInputSelect(hass.Hass):
             labels = list(list(zip(*data))[0])
             self.call_service('input_select/set_options', entity_id=self._movie_inputselect_entity, options=["Select a movie to watch"] + labels)
             self.set_state(self._movie_inputselect_entity, attributes={"friendly_name": 'Recent Movies', "icon": 'mdi:movie'})
+            self.listen_state(self._change_selected_option, self._movie_inputselect_entity)
         elif method == 'VideoLibrary.GetRecentlyAddedEpisodes':
             values = list(filter(lambda r: not r['lastplayed'], result['episodes']))[:self._series_max_entries]
             data = [('{} - {}'.format(r['showtitle'], r['label']), ('TVSHOW', r['file'])) for r in values]
@@ -37,6 +36,7 @@ class DynamicKodiInputSelect(hass.Hass):
             labels = list(list(zip(*data))[0])
             self.call_service('input_select/set_options', entity_id=self._series_inputselect_entity, options=["Select an episode to watch"] + labels)
             self.set_state(self._series_inputselect_entity, attributes={"friendly_name": 'Recent TvShows', "icon": 'mdi:filmstrip'})
+            self.listen_state(self._change_selected_option, self._series_inputselect_entity)
 
     def _change_selected_option(self, entity, attribute, old, new, kwargs):
         selected = self._ids_options[new]
