35bb1b741eb8e05708682572e89a8d5ea0ffa561
jkadivnik <johnny.kadivnik@esfds.com>
2018-12-12 23:35:55 +0100

    Differentiated between series and movie max entries.

--- appdaemon/apps/dynamic_kodi_inputselect.py
+++ appdaemon/apps/dynamic_kodi_inputselect.py
@@ -10,7 +10,8 @@ class DynamicKodiInputSelect(hass.Hass):
         self._movie_inputselect_entity = self.args.get('movie_inputselect', str)
         self._series_inputselect_entity = self.args.get('series_inputselect', str)
         self._kodi_entity = self.args.get('kodi_entity', str)
-        self._max_entries = int(self.args.get('max_entries', 10))
+        self._movies_max_entries = int(self.args.get('movies_max_entries', 10))
+        self._series_max_entries = int(self.args.get('movies_max_entries', 10))
 
         self.listen_event(self._receive_kodi_result,EVENT_KODI_CALL_METHOD_RESULT)
         self.listen_state(self._change_selected_option, self._movie_inputselect_entity)
@@ -23,19 +24,19 @@ class DynamicKodiInputSelect(hass.Hass):
 
         assert event_id == EVENT_KODI_CALL_METHOD_RESULT
         if method == 'VideoLibrary.GetRecentlyAddedMovies':
-            values = result['movies'][:self._max_entries]
+            values = result['movies'][:self._movies_max_entries]
             data = [('{} ({})'.format(r['label'], r['year']), ('MOVIE', r['file'])) for r in values]
             self._ids_options.update(dict(zip(*zip(*data))))
             labels = list(list(zip(*data))[0])
             self.call_service('input_select/set_options', entity_id=self._movie_inputselect_entity, options=["Select a movie to watch"] + labels)
             self.set_state(self._movie_inputselect_entity, attributes={"friendly_name": 'Recent Movies', "icon": 'mdi:movie'})
         elif method == 'VideoLibrary.GetRecentlyAddedEpisodes':
-            values = list(filter(lambda r: not r['lastplayed'], result['episodes']))[:self._max_entries]
+            values = list(filter(lambda r: not r['lastplayed'], result['episodes']))[:self._series_max_entries]
             data = [('{} - {}'.format(r['showtitle'], r['label']), ('TVSHOW', r['file'])) for r in values]
             self._ids_options.update(dict(zip(*zip(*data))))
             labels = list(list(zip(*data))[0])
             self.call_service('input_select/set_options', entity_id=self._series_inputselect_entity, options=["Select an episode to watch"] + labels)
-            self.set_state(self._series_inputselect_entity, attributes={"friendly_name": 'Recent TvShows', "icon": 'mdi:play-circle'})
+            self.set_state(self._series_inputselect_entity, attributes={"friendly_name": 'Recent TvShows', "icon": 'mdi:filmstrip'})
 
     def _change_selected_option(self, entity, attribute, old, new, kwargs):
         selected = self._ids_options[new]
