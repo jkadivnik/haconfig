8d66d21298efff3bbb0983cb7e72e480edd9b694
jkadivnik <johnny.kadivnik@esfds.com>
2018-12-12 23:31:24 +0100

    Got it working!

--- appdaemon/apps/dynamic_kodi_inputselect.py
+++ appdaemon/apps/dynamic_kodi_inputselect.py
@@ -1,11 +1,8 @@
 import appdaemon.plugins.hass.hassapi as hass
 
-ENTITY = 'input_select.kodi_results'
-MEDIA_PLAYER = 'media_player.kodi'
-DEFAULT_ACTION = "Nothing to do"
-MAX_RESULTS = 20
+EVENT_KODI_CALL_METHOD_RESULT = 'kodi_call_method_result'
 
-class DynamicKodiInputSelect(appapi.AppDaemon):
+class DynamicKodiInputSelect(hass.Hass):
     """AppDaemon app to dynamically populate an `input_select`."""
     _ids_options = None
 
@@ -16,7 +13,8 @@ class DynamicKodiInputSelect(appapi.AppDaemon):
         self._max_entries = int(self.args.get('max_entries', 10))
 
         self.listen_event(self._receive_kodi_result,EVENT_KODI_CALL_METHOD_RESULT)
-        # Input select:
+        self.listen_state(self._change_selected_option, self._movie_inputselect_entity)
+        self.listen_state(self._change_selected_option, self._series_inputselect_entity)
         self._ids_options = {"Kodi Results": None}
 
     def _receive_kodi_result(self, event_id, payload_event, *args):
@@ -25,19 +23,18 @@ class DynamicKodiInputSelect(appapi.AppDaemon):
 
         assert event_id == EVENT_KODI_CALL_METHOD_RESULT
         if method == 'VideoLibrary.GetRecentlyAddedMovies':
-            log.info
             values = result['movies'][:self._max_entries]
             data = [('{} ({})'.format(r['label'], r['year']), ('MOVIE', r['file'])) for r in values]
             self._ids_options.update(dict(zip(*zip(*data))))
             labels = list(list(zip(*data))[0])
-            self.call_service('input_select/set_options', entity_id=self._movie_inputselect_entity, options=["Movies not filled"] + labels)
+            self.call_service('input_select/set_options', entity_id=self._movie_inputselect_entity, options=["Select a movie to watch"] + labels)
             self.set_state(self._movie_inputselect_entity, attributes={"friendly_name": 'Recent Movies', "icon": 'mdi:movie'})
         elif method == 'VideoLibrary.GetRecentlyAddedEpisodes':
             values = list(filter(lambda r: not r['lastplayed'], result['episodes']))[:self._max_entries]
             data = [('{} - {}'.format(r['showtitle'], r['label']), ('TVSHOW', r['file'])) for r in values]
             self._ids_options.update(dict(zip(*zip(*data))))
             labels = list(list(zip(*data))[0])
-            self.call_service('input_select/set_options', entity_id=self._series_inputselect_entity, options=["Series not filled"] + labels)
+            self.call_service('input_select/set_options', entity_id=self._series_inputselect_entity, options=["Select an episode to watch"] + labels)
             self.set_state(self._series_inputselect_entity, attributes={"friendly_name": 'Recent TvShows', "icon": 'mdi:play-circle'})
 
     def _change_selected_option(self, entity, attribute, old, new, kwargs):
